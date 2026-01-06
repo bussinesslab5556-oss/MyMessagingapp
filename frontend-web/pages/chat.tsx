ba5e9e2283e816e57f90d2d37fb86619e18c7e8d
bussinesslab5556-oss <bussinesslab5556@gmail.com>
2026-01-06 17:33:26 +0600

    Update chat.tsx new1

---
+++ frontend-web/pages/chat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ChatPage = () => {
+  return (
+    <div>
+      <h1>Chat Screen</h1>
+      {/* আপনার চ্যাট কম্পোনেন্ট এখানে থাকবে */}
+    </div>
+  );
+};
+
+// এই লাইনটি থাকা বাধ্যতামূলক
+export default ChatPage;
